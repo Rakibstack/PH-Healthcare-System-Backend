149de62fa103d8707e0c6bd36e699c31e3f50463
Rakibstack <rh6039511@gmail.com>
2026-09-02 22:43:44 +0600

    feat: Implement Tester_Admin and Tester_Doctor  Seeding Function

--- src/app/utils/seed.ts
+++ src/app/utils/seed.ts
@@ -50,3 +50,93 @@ export const seedSuperAdmin = async () => {
     });
   }
 };
+export const seedTesterAdmin = async () => {
+  try {
+    const isTesterAdminExist = await prisma.user.findUnique({
+      where: {
+        email : config.tester_admin_email,
+      },
+    });
+
+    if (isTesterAdminExist) {
+      console.log("Tester Admin Already Exists");
+      return;
+    }
+    const name = config.tester_admin_name;
+    const email = config.tester_admin_email;
+    const password = config.tester_admin_password;
+
+    if (!name || !email || !password) {
+      throw new Error("Tester Admin Name,Email,Password Is Missing In ENV");
+    }
+
+    const hashPassword = await bcrypt.hash(
+      password,
+      Number(config.bcrypt_salt_rounds),
+    );
+
+    const testerAdmin = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+        emailVerified: true,
+        role: Role.ADMIN,
+        needPasswordChange: false,
+      },
+    });
+    console.log("tester admin created : ", testerAdmin);
+  } catch (error) {
+    console.log("Error Seeding Tester Admin : ", error);
+    await prisma.user.delete({
+      where: {
+        email: config.tester_admin_email,
+      },
+    });
+  }
+};
+export const seedTesterDoctor = async () => {
+  try {
+    const isTesterDoctorExist = await prisma.user.findUnique({
+      where: {
+        email : config.tester_doctor_email,
+      },
+    });
+
+    if (isTesterDoctorExist) {
+      console.log("Tester Doctor Already Exists");
+      return;
+    }
+    const name = config.tester_doctor_name;
+    const email = config.tester_doctor_email;
+    const password = config.tester_doctor_password;
+
+    if (!name || !email || !password) {
+      throw new Error("Tester Doctor Name,Email,Password Is Missing In ENV");
+    }
+
+    const hashPassword = await bcrypt.hash(
+      password,
+      Number(config.bcrypt_salt_rounds),
+    );
+
+    const testerDoctor = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+        emailVerified: true,
+        role: Role.DOCTOR,
+        needPasswordChange: false,
+      },
+    });
+    console.log("tester doctor created : ", testerDoctor);
+  } catch (error) {
+    console.log("Error Seeding Tester Doctor : ", error);
+    await prisma.user.delete({
+      where: {
+        email: config.tester_doctor_email,
+      },
+    });
+  }
+};
